3826c3e870329347592b090b0f3feda93aee3c70
LimYunhwi <88yhtserof@naver.com>
2021-07-01 22:48:06 +0900

    1 Hello! 아이폰 앱 02-4~7
    1. 레이블 및 버튼의 아울렛 변수, 액션 함수 지정
    2. 버튼 클릭시 작동될 액션 함수 코드 작성

--- StudyIOS/ViewController.swift
+++ StudyIOS/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var IdHello: UILabel! //출력 레이블용 아울렛 변수
+    @IBOutlet var txtName: UITextField!  //이름 입력용 아울렛 변수
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnSend(_ sender: UIButton) {
+        //"Hello, "라는 문자열과 txtName.text의 문자열을 결합하여 IdHello.txt에 넣음
+        //문자열을 합칠 때는 + 연산자를 사용한다.
+        IdHello.text="Hello, "+txtName.text!
+    }
 }
 
