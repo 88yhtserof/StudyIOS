60105f525b7f3d168ed7341322f4dc51d6fc0a52
LimYunhwi <88yhtserof@naver.com>
2021-07-03 00:05:46 +0900

    1 Hello! 아이폰 앱 02-review2
    1.strong/weak 속성 내용 정리
    2. 오류 수정

--- StudyIOS/ViewController.swift
+++ StudyIOS/ViewController.swift
@@ -6,11 +6,15 @@ class ViewController: UIViewController {
     @IBOutlet var txtName: UITextField!  //이름 입력용 아울렛 변수
     
     @IBOutlet var reviewOutput: UILabel!
-    @IBOutlet var reviewaInputValue: UITextField!
+    @IBOutlet var reviewTxt: UITextField!
     /*
      IB는 Interface Builder의 약자로,
      @IB로 시작되는 변수나 함수는 인터페이스 빌더와 관려된 변구나 함수라는 것을 의미합니다.
      @IBOulet은 객체를 소스 코드에서 참조하기 위해 사용하는 키워드로, 주로 객체의 속성을 제어하는데 사용
+     
+     strong/weak : 메모리 회수 정책을 나타내는 키워드 -> 목적: 메모리 관리 이슈
+     strong으로 선언된 변수는 다른 곳에서 참조하고 있을 경우 메모리에서 제거되지 않는다.
+     weak으로 선언된 변수는 다른 곳에서 참조하고 있더라도 시스템이 임의적으로 메모리에서 제거할 수 있다.
      */
     
     
@@ -26,7 +30,7 @@ class ViewController: UIViewController {
         IdHello.text="Hello, "+txtName.text!
     }
     @IBAction func sendString(_ sender: UIButton) {
-        reviewOutput.text="Review, "+reviewaInputValue.text!
+        reviewOutput.text="Review, "+reviewTxt.text!
         //!는 옵셔널 변수의 강제 언래핑을 의미한다.
     }
 }
