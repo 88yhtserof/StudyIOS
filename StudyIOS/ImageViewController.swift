843112402a2ff1a7b4df05b193c88660f63c9c75
LimYunhwi <88yhtserof@naver.com>
2021-07-08 16:48:26 +0900

    2 뷰 기반 프로그램 직접 만들기 3-4
    1. 아울렛 변수와 액션 함수 추가하기

--- StudyIOS/ImageViewController.swift
+++ StudyIOS/ImageViewController.swift
@@ -1,8 +1,18 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
     }
 }
