e634bfc50acff85fc6f19c9be7c8e18d4f14804f
LimYunhwi <88yhtserof@naver.com>
2021-07-12 14:32:14 +0900

    2.4 데이터 피커 사용해 날짜 선택하기 4-4 ~ 4-5
    1. 아울렛 변수와 액션함수 추가하기
    2. 선택한 날자와 시간 출력할 코드 작성하기

--- StudyIOS/DatePickerViewController.swift
+++ StudyIOS/DatePickerViewController.swift
@@ -6,8 +6,20 @@
 import UIKit
 
 class DatePickerViewController: UIViewController {
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
     override func viewDidLoad() {
         // Do any additional setup after loading the view.
         super.viewDidLoad()
+    }
+    
+    //데이터피커 액션함수는 데이터 피커를 선택했을때 실행된다.
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let dataPickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyy-MM-dd a hh:mm EEEE" //날짜가 출력될 형식 지정
+        lblPickerTime.text =
+            "선택시간: " + formatter.string(from: dataPickerView.date)
     }
 }
