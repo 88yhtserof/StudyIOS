66a63fe5dfc53e563883e4af5b7dd0295cd1a1dd
LimYunhwi <88yhtserof@naver.com>
2021-07-12 13:37:33 +0900

    2.4  데이터 피커 사용해 날짜 선택하기 4-1~4-3
    1.데이터 피커 객체 공부
    2. 레이아웃 구현

---
+++ StudyIOS/DatePickerViewController.swift
@@ -0,0 +1,13 @@
+/*
+ 데이터 피커(DataPicker): 날짜와 시간을 선택할 수 있게 해주는 객체
+ 종류 - 시간형/ 날짜형/ 날짜&시간/ 카운트다운형
+ */
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    override func viewDidLoad() {
+        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+    }
+}
