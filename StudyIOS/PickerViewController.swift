de2d43f81f8a652436b5b70b9acc6b9768a23635
LimYunhwi <88yhtserof@naver.com>
2021-07-19 01:04:01 +0900

    2.5 피커 뷰 사용해 원하는 항목 선택하기 05-5
    1. 변수 및 상수 추가
    2. 델리게이트 메서드 작성

--- StudyIOS/PickerViewController.swift
+++ StudyIOS/PickerViewController.swift
@@ -1,8 +1,39 @@
-//
-//  PickerViewController.swift
-//  StudyIOS
-//
-//  Created by 임윤휘 on 2021/07/18.
-//
+import UIKit
 
-import Foundation
+class PickerViewController: UIViewController,
+                            UIPickerViewDelegate,
+                            UIPickerViewDataSource {
+    let MAX_ARRAY_NUM = 10
+    let PICKER_VIEW_COLUMN = 1
+    var imageFileName = ["WallaceAndGromit_putArmsAround.jpg",
+                        "WallaceAndGromit_playingMusic.jpg",
+                        "WallaceAndGromit_playingMusic.png"]
+    
+    
+    @IBOutlet var pickerImage: UIPickerView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var lblImageFileName: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //델리게이트 메서드 delegate
+    //row 가로 행 column 세로 열
+    //returns the number of 'columns' to display
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    //return the # of row in each component..
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component:Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int, forComponent component: Int) -> String?{
+        return imageFileName[row]
+    }
+}
