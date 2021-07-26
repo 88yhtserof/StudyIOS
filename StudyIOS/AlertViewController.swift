1182031e52721935a637129478dbb291bb3eb0b1
LimYunhwi <88yhtserof@naver.com>
2021-07-26 23:02:51 +0900

    2.6 얼럿 사용해 경고 메세지 표시하기
    1. 얼럿(Alert) 개념 공부
    2. AlertViewController 생성

---
+++ StudyIOS/AlertViewController.swift
@@ -0,0 +1,15 @@
+/*
+ 얼럿(Alert):
+ 사용자에게 중요한 알림이나 경고 메세지를 나타내야 할 때 주로 사용한다.
+ 사용자의 주의를 집중시키는 경고로 마무리할 수도 있고 후속 조치를 취할 수도 있다.
+ */
+import UIKit
+
+class AlertViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
