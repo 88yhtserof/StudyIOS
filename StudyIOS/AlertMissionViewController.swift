ca27e2bd4436019efc322dda635371c3f2d557d2
LimYunhwi <88yhtserof@naver.com>
2021-07-27 16:53:03 +0900

    2.6 얼럿 사용해 경고 메세지 표시하기 Mission
    1. 현재 시간과 선택 시간 비교 후 알림 메세지 띄우기

--- StudyIOS/AlertMissionViewController.swift
+++ StudyIOS/AlertMissionViewController.swift
@@ -13,6 +13,7 @@ class AlertMissionViewController: UIViewController {
     
     let timeSelector: Selector = #selector(AlertMissionViewController.updateTime)//타이머가 구동되면 호출할 함수 지정
     let interval:Double = 1.0 //현재시간이 갱신될 간격
+    var alamTime:String = String()
     
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblSelectTime: UILabel!
@@ -33,13 +34,27 @@ class AlertMissionViewController: UIViewController {
         formatter.dateFormat = "yyyy년MM월dd일 a hh시mm분"
         
         lblSelectTime.text = formatter.string(from: selectDate)
+        alamTime = formatter.string(from: selectDate)
     }
     
     @objc func updateTime(){
         let date: NSDate = NSDate() //현재 시간을 받아오는 클래스 객체 생성
-        
+        var currentTime:String = String()
         let formatter = DateFormatter()
+        let compareFormatter = DateFormatter()
+        
         formatter.dateFormat = "yyyy년MM월dd일 a hh시mm분ss초"
+        compareFormatter.dateFormat = "yyyy년MM월dd일 a hh시mm분"
         lblCurrentTime.text = formatter.string(from: date as Date) //현재시간 출력
+        currentTime = compareFormatter.string(from: date as Date)
+        
+        if alamTime == currentTime {
+            let alamAlert = UIAlertController(title: "알림", message: "설정된 시간입니다.", preferredStyle: UIAlertController.Style.alert)
+            
+            let alamAction = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
+            
+            alamAlert.addAction(alamAction)
+            present(alamAlert, animated: true, completion: nil)
+        }
     }
 }
