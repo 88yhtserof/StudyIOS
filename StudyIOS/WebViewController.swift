baa2cd3d705b17c46248402f58be5d7fca32e6b2
LimYunhwi <88yhtserof@naver.com>
2021-07-31 19:44:04 +0900

    2.7 웹 뷰로 간단한 웹 브라우저 만들기 1 ~ 3
    1. 웹 뷰란?
    2.ViewController 생성 및 연결
    3. 레이아웃 배치

---
+++ StudyIOS/WebViewController.swift
@@ -0,0 +1,13 @@
+/*
+ 웹 뷰란?
+ 웹 콘텐츠를 뷰 형태로 보여 주는 앱. HTML로 작성된 홈페이지를 표시할 수 있다.
+ 직접 인터넷에 연결된 주소를 입력하여 홈페이지에 접속할 수 있을 뿐만 아니라 미리 저장된 HTML 파일을 읽어들여 표시 가능
+ */
+
+import UIKit
+
+class WebViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
