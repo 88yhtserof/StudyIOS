48bf62938c0e49f1dd4b528ff0801f7733c469db
LimYunhwi <88yhtserof@naver.com>
2021-08-04 23:36:42 +0900

    2.7 웹 뷰로 간단한 웹 브라우저 만들기 Mission
    1. 액티비티 인디케이터 델리게이트 메서드 작성

--- StudyIOS/WebMissionViewController.swift
+++ StudyIOS/WebMissionViewController.swift
@@ -12,7 +12,7 @@
 import UIKit
 import WebKit
 
-class WebMissionViewController: UIViewController {
+class WebMissionViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet var txtUrl: UITextField! //텍스트 필드에 적힌 text 속정을 제어해야 하므로 아울렛 변수 추가
     @IBOutlet var myWebView: WKWebView! //웹 뷰의 내용, 즉 속성을 제어해야하므로 아울렛 변수 추가
     @IBOutlet var MyActivityIndicator: UIActivityIndicatorView!//인티케이터 hide 속성을 제어해야 하므로 아울렛 변수 추가
@@ -20,11 +20,34 @@ class WebMissionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        myWebView.navigationDelegate = self //self는 해당 뷰를 의미한다.
+        
         //초기 화면 설정
         let filePath = Bundle.main.path(forResource: "htmlView", ofType: "html")
         let fileUrl = URL(fileURLWithPath: filePath!)
         let fileRequest = URLRequest(url: fileUrl)
         myWebView.load(fileRequest)
+    }
+    
+    //didCommit을 인수로 갖는 메서드
+    //로딩 중인지 확인하는 델리게이트 메서드
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        MyActivityIndicator.startAnimating()
+        MyActivityIndicator.isHidden = false
+    }
+    
+    //didFinish를 인수로 갖는 메서드
+    //로딩이 끝났음을 알리는 델리게이트 메서드
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        MyActivityIndicator.stopAnimating()
+        MyActivityIndicator.isHidden = true
+    }
+    
+    //didFail과 withError을 인수로 갖는 델리게이트 메서드
+    //로딩 중 오류가 발생했음을 알리는 델리게이트 메서드
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        MyActivityIndicator.stopAnimating()
+        MyActivityIndicator.isHidden = true
     }
     
     //웹 뷰에 웹 페이지를 로드하기 위한 함수
