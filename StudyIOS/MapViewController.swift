90569508ecaf5bc05e512a514fc816ed09f0bca3
LimYunhwi <88yhtserof@naver.com>
2021-08-11 17:08:18 +0900

    2.8 맵 뷰로 지도 나타내기 7~8
    1. 위도와 경도로 원하는 위치 표시하기
    2. 위치 정보 추출해 텍스트로 표시하기

--- StudyIOS/MapViewController.swift
+++ StudyIOS/MapViewController.swift
@@ -23,6 +23,51 @@ class MapViewController: UIViewController, CLLocationManagerDelegate{
         myMap.showsUserLocation = true
     }
     
+    //지도를 나타내기 위한 함수
+    func goLocation(latitudeValue: CLLocationDegrees,
+                    longitudeValue: CLLocationDegrees, dleta span : Double){
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        myMap.setRegion(pRegion, animated: true)
+    }
+    
+    //위치가 업데이트되었을 때 지도에 위치를 나타내기 위한 함수
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        let pLocation = locations.last //위치가 업데이트되면 먼저 마지막 위치 값을 찾아낸다.
+        goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, dleta: 0.01)
+        /*
+         마지막 위치의 위도와 경도 값을 가지고 앞에서 만든 goLocation함수를 호출한다.
+         이때 delta 값은 지도의 크기를 정하는데, 값이 작을 수록 확대되는 효과가 있다.
+         delta를 0.01로 하였으니 1의 값보다 100배 확대해서 보여줄 것이다.
+         */
+        
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
+            (placemarks, error) -> Void in
+            let pm = placemarks!.first //placemarks의 첫 부분만 pm 상수로 대입한다.
+            let country = pm!.country //pm 상수에서 나라 값을 country에 대입한다.
+            var address:String = country!// address네 country 상수에 저장된 나라 값을 대입한다.
+            
+            //pm 상수에 지역 값이 존배하면 address 문자열에 추가한다.
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.locality!
+            }
+            //pm 상수에 도로 값이 존재하면 address 문자열에 추가한다.
+            if pm!.thoroughfare != nil {
+                address += " "
+                address += pm!.thoroughfare!
+            }
+            
+            self.lblLocationInfo1.text = "현재 위치"
+            self.lblLocationInfo2.text = address
+            
+            //위치가 업데이트되는 것을 멈추게 한다.
+            self.locationManager.stopUpdatingLocation()
+            
+        })
+    }
+    
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
     }
 }
