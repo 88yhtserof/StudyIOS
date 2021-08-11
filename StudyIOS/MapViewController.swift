f5b395af29f9ccb5b1e99bf2ad7424d7314235cb
LimYunhwi <88yhtserof@naver.com>
2021-08-11 15:09:03 +0900

    2.8 맵 뷰로 지도 나타내기 4
    1. 아울렛 변수와 액션 함수 추가하기

--- StudyIOS/MapViewController.swift
+++ StudyIOS/MapViewController.swift
@@ -1,7 +1,15 @@
 import UIKit
+import MapKit
+//MapKit은 지도를 확대, 축소, 및 이동하는 등 지도에 관한 여러 기능을 제공한다.
 
 class MapViewController: UIViewController {
+    @IBOutlet var myMap: MKMapView!
+    @IBOutlet var lblLocationInfo1: UILabel!
+    @IBOutlet var lblLocationInfo2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
     }
 }
