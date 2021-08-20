5bd91d0f09af42789f510d31121692ac4517e8b3
LimYunhwi <88yhtserof@naver.com>
2021-08-20 15:21:32 +0900

    3.9 페이지 이동하기 - 페이지 컨트롤 Mission
    1. 아울렛 변수, 액션 함수 주석 추가
    2. 이미지 배열 추가
    3. 페이지 컨트롤 액션함수 코드 작성
    4. 초기 화면 설정 구현

--- StudyIOS/PageControlMissionViewController.swift
+++ StudyIOS/PageControlMissionViewController.swift
@@ -1,15 +1,32 @@
 import UIKit
 
+var images = ["WallaceAndGromit_putArmsAround.jpg",
+              "WallaceAndGromit_playingMusic.jpg",
+              "WallaceAndGromit_face.png"]
+
+
 class PageControlMissionViewController: UIViewController {
-    @IBOutlet var lblPageIndex: UILabel!
-    @IBOutlet var imgView: UIImageView!
-    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var lblPageIndex: UILabel! //text 속성 사용
+    @IBOutlet var imgView: UIImageView! //image 속성 사용
+    @IBOutlet var pageControl: UIPageControl! //current, 전체 페이지 수 설정, 색 설정 속성 사용
     
+    //뷰 로드 시 호출되는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //시작 화면 설정
+        imgView.image = UIImage(named: images[0])
+        lblPageIndex.text = "1"
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.green
     }
     
     //Evemt - value Change : 페이지가 변하면 호출된다.
     @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+        lblPageIndex.text = String(pageControl.currentPage + 1)
     }
 }
