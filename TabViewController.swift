8c1c03418d376003c38dea5c04bd663a99ac0d7c
LimYunhwi <88yhtserof@naver.com>
2021-08-23 16:45:31 +0900

    3.10 탭 바 컨트롤러 이용해 여러 개의 뷰 넣기 4~7
    1. 첫 화면을 위한 스토리보드 수정하기
    2. 버튼으로 뷰 전환을 위한 함수 추가
    3. 버튼으로 뷰 전환하기
    4,전체 코드 확인 후 주석 추가

--- TabViewController.swift
+++ TabViewController.swift
@@ -4,4 +4,14 @@ class TabViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        //이 숫자는 탭 바 아이템 순서이다. 0~
+        tabBarController?.selectedIndex = 1 //1-이미지 뷰 택으로 이동
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        //이 숫자는 탭 바 아이템 순서이다. 0~
+        tabBarController?.selectedIndex = 2 //2-데이트 피커 뷰 탭으로 이동
+    }
 }
