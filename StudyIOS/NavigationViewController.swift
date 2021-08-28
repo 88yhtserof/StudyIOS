e5941224fa345538ee7de576051f4a45a965b931
LimYunhwi <88yhtserof@naver.com>
2021-08-28 17:11:04 +0900

    3.11 내비게이션 컨트롤러 이용해 화면 전환하기 0~2
    1. 내비게이션 컨트롤러란? 탭 바 컨트롤러와의 차이
    2. 내비게이션 컨트롤러 앱을 위한 기본 환경 구성하기

---
+++ StudyIOS/NavigationViewController.swift
@@ -0,0 +1,13 @@
+/*
+ 내비게이션 컨트롤러
+ : 화면을 전환할 때 연관성이 없거나 데이터를 서로 주고 받아야 하는 경우에 사용. 즉, 한 주제가 이어지는 화면에 사용
+ + 탭 바 컨트롤러: 각 화면이 서로 연관성이 없는 경우에 사용. 즉, 각 화면 별 주제가 있는 경우
+ 내비게이션 컨트롤러를 스토리보드에 추가하기 -> 해당 뷰 컨트롤러 클릭 후 [Editor/Enbed in/NavigationController]
+ */
+import UIKit
+
+class NavigationViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
