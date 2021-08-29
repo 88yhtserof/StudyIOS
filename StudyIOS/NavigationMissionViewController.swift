28f546b87571296948b198d68be0ab347d080956
LimYunhwi <88yhtserof@naver.com>
2021-08-29 19:46:10 +0900

    3.11 내비게이션 컨트롤러 이용해 화면 전환하기 Mission
    1. 내비게이션 컨트롤러 추가
    2. '메인화면' 레이아웃 구성
    3. '수정화면' 추가 및 레이아웃 구성

---
+++ StudyIOS/NavigationMissionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationMissionViewController.swift
+//  StudyIOS
+//
+//  Created by 임윤휘 on 2021/08/29.
+//
+
+import UIKit
+
+class NavigationMissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
