206a6886f2340400216713360079f2e6cc01b910
LimYunhwi <88yhtserof@naver.com>
2021-08-30 00:44:41 +0900

    3.11 내비게이션 컨트롤러 이용해 화면 전환하기 Mission
    1. 뷰 전환과 함께 메시지 전달하기 - 뷰 전환과 함께 메시지 전달하기
    2. 뷰 전환과 함께 메시지 전달하기 - '메인화면' -> '수정화면' 데이터 전달
    3. 뷰 전환과 함께 메시지 전달하기 - '수정화면' -> '메인화면' 화면 전환 및 데이터 전달

--- StudyIOS/EditMissionViewController.swift
+++ StudyIOS/EditMissionViewController.swift
@@ -7,15 +7,39 @@
 
 import UIKit
 
-class EditMissionViewController: UIViewController {
+protocol EditMissionDelegate {
+    //EditViewController에서 함수를 호출하며 메시지를 전달해 주는데, 이 메시지의 문자열 값을 '메인화면'의 텍스트 필드에 텍스트로 보여준다.
+    //즉, '수정화면'에서 '메인화면'으로 데이터 전달
+    func didMessageEditDone(_ controller: EditMissionViewController, message: String)
+}
 
+class EditMissionViewController: UIViewController {
+    
+    var textMessage: String = ""  //'메인화면'에서 Text Fielf의 text를 직접 제어할 수 없으므로 변수를 생성해 '메인화면'에서 해당 변수를 제어할 수 있도록 한다.
+    var delegate:EditMissionDelegate?
+    
+    
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swIsOn: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        txMessage.text = textMessage//변수 message값은 txMessage의 text에 할당해 출력되도록 한다.
     }
     
 
+    @IBAction func btnDone(_ sender: UIButton) {
+        if delegate != nil {
+            delegate!.didMessageEditDone(self, message: txMessage.text!)
+        }
+        
+        _ = navigationController?.popViewController(animated: true)
+        //내비게이션 스택에서 top 뷰 컨트롤러를 삭제 후 반환한다(pop). 그리고 화면을 갱신한다.
+        //와일드 카드 식별자는 반환값은 있지만 사용하지 않을 때 사용한다
+    }
+    
     /*
     // MARK: - Navigation
 
--- StudyIOS/NavigationMissionViewController.swift
+++ StudyIOS/NavigationMissionViewController.swift
@@ -7,15 +7,29 @@
 
 import UIKit
 
-class NavigationMissionViewController: UIViewController {
-
+class NavigationMissionViewController: UIViewController, EditMissionDelegate {
+    
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    //해당 세그웨이가 해당 뷰 컨트롤러로 전환되기 직전에 호출되는 함수이며 데이터 전달을 위해 사용한다.
+    //'메인화면'-> '수정화면' 화면 전환 및 데이터 전달
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editMissionViewController = segue.destination as! EditMissionViewController //세그웨이의 도착 뷰 컨트롤러를 EditMissionViewController로 한다.
+        editMissionViewController.textMessage = txMessage.text!
+        
+        editMissionViewController.delegate = self
+    }
+    
+    func didMessageEditDone(_ controller: EditMissionViewController, message: String) {
+        txMessage.text = message
+    }
     /*
     // MARK: - Navigation
 
