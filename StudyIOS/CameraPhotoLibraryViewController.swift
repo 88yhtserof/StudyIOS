7b0c9b5aa8431e85bc8abfed55fc40124f33b53e
LimYunhwi <88yhtserof@naver.com>
2021-09-16 12:40:43 +0900

    4.15 카메라와 포토 라이브러리에서 미디어 가져오기 3~4.4
    1.아울렛 변수와 액션 함수 추가하기
    2. 카메라와 포토 라이브러리 앱 기능 구현하기 - 헤더 파일과 프로토콜 추가, 변수 추가
    3. UIKit 와 MobileCoreServices을 import하는 목적 정리

--- StudyIOS/CameraPhotoLibraryViewController.swift
+++ StudyIOS/CameraPhotoLibraryViewController.swift
@@ -5,25 +5,38 @@
 //  Created by 임윤휘 on 2021/09/15.
 //
 
+/*
+ UIKit: 우리가 오브젝트 라이브러리에서 추가하는 모든 객체의 클래스가 모여있다.
+ MobileCoreServices: iOS에서 사용할 모든 "데이터 타입"들이 정의되어 있는 헤더 파일. 그러므로 "미디어 타입"을 사용하기 위해 필요
+ */
 import UIKit
+import MobileCoreServices
 
-class CameraPhotoLibraryViewController: UIViewController {
-
+class CameraPhotoLibraryViewController: UIViewController,
+                                        UINavigationControllerDelegate,
+                                        UIImagePickerControllerDelegate {
+    @IBOutlet var imgView: UIImageView!
+    
+    let imagePicker: UIImagePickerController! = UIImagePickerController() //UIImagePickerController의 인스턴스 변수 생성
+    var captureImage: UIImage! //촬영을 하거나 포토 라이브러리에서 불러온 사진(Image)을 저장할 변수
+    var videoURL: URL!  //녹화한 비디오의 URL을 저장할 변수
+    var flagImageSave = false //이미지 저장 여부를 나타낼 변수
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnCaptureImageFromCamera(_ sender: UIButton) {
     }
-    */
-
+    
+    @IBAction func btnLoadImageFromLibrary(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnRecordVideoCamera(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadVideoFromLibrary(_ sender: UIButton) {
+    }
 }
